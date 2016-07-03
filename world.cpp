c08701584e69dad27caeb5a35193bc57e29788e3
Phillip Tennen <phillip.ennen@gmail.com>
2016-07-03 04:40:28 -0600

    Inform each player of game outcome

--- world.cpp
+++ world.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-#define NUM_PLAYERS 2
+#define NUM_PLAYERS 64
 
 enum PrisonerResponse : bool {
 	Cooperate = true,
@@ -22,32 +22,35 @@ class Player {
 public:
 	int score;
 	string name;
+	string type;
 	
 	Player() { 
-		//static int existing_player_count = 0;
+		static int existing_player_count = 0;
 
 		score = 0;
-/*
+		
 		ostringstream oss;
-		oss << "Player #" << "nan";
+		oss << existing_player_count;
 		name = oss.str();
-*/
-		name = "Player";
-		//existing_player_count++;
+		type = "Random";
+		
+		existing_player_count++;
 	}
 
 	PrisonerResponse prisoners_dilemna() {
-		return Cooperate;
+		return (PrisonerResponse)(rand() % 2);
 	}
 
-	void inform_outcome(GameOutcome outcome);
+	void inform_outcome(GameOutcome outcome) {}
 };
 
-class Grudger : Player {
+class Grudger : public Player {
 private:
 	bool grudged = false;
 public:
-	Grudger() {}
+	Grudger() {
+		type = "Grudger";
+	}
 
 	PrisonerResponse prisoners_dilemna() {
 		if (grudged) return Defect;
@@ -70,9 +73,16 @@ GameOutcome determine_outcome(bool me_coop, bool you_coop) {
 }
 
 void play_game(Player* me, Player* you) {
+	//seed game for any players that use rand
+	//srand(time(0));
+
 	PrisonerResponse me_coop = me->prisoners_dilemna();
 	PrisonerResponse you_coop = you->prisoners_dilemna();
 	GameOutcome outcome = determine_outcome(me->prisoners_dilemna(), you->prisoners_dilemna());
+
+	//inform each player of game outcome
+	me->inform_outcome(outcome);
+	you->inform_outcome(outcome);
 
 	//if players cooperated, both get 1 point
 	if (outcome == Friends) {
@@ -83,16 +93,16 @@ void play_game(Player* me, Player* you) {
 	else {
 		//if someone defected alone, they get 3 points
 		if (outcome == Sabotage && !me_coop) {
-			cout << me->name << "sabotaged." << endl;
+			cout << me->name << " sabotaged." << endl;
 			me->score += 3;
 		}
 		else if (outcome == Sabotage && !you_coop) {
-			cout << you->name << "sabotaged." << endl;
+			cout << you->name << " sabotaged." << endl;
 			you->score += 3;
 		}
 		//if both defected, they get punished 
 		else {
-			cout << me->name << ", " << you->name << "both defected." << endl;
+			cout << me->name << ", " << you->name << " both defected." << endl;
 			me->score--;
 			you->score--;
 		}
@@ -101,16 +111,17 @@ void play_game(Player* me, Player* you) {
 }
 
 void score_display(Player* (&players)[NUM_PLAYERS]) {
+	cout << "--------Scoreboard--------" << endl;
 	for (unsigned i = 0; i < NUM_PLAYERS; i++) {
 		Player* player = players[i];
 
-		cout << "Player: " << player->name << ", Score: " << player->score << endl;
+		cout << player->type << " (" << player->name << "): " << player->score << endl;
 	}
 }
 
 void world_play(Player* (&players)[NUM_PLAYERS]) {
 	//run a life for every player and add to their cumulative score
-	for (unsigned i = 0; i < NUM_PLAYERS - 1; i++) {
+	for (unsigned i = 0; i < NUM_PLAYERS - 1; i+= 2) {
 		//TODO match players randomly, while only playing one player per turn
 		//let this player run a game
 		play_game(players[i], players[i+1]);
@@ -121,10 +132,11 @@ int main(void) {
 	Player* players[NUM_PLAYERS];
 	//initialize players
 	for (unsigned i = 0; i < NUM_PLAYERS; i++) {
-		players[i] = new Player();
+		if (i % 2 == 0) players[i] = new Grudger();
+		else players[i] = new Player();
 	}
 
-	for (unsigned i = 0; i < 100; i++) {
+	for (unsigned i = 0; i < 1; i++) {
 		//run world for a lifetime
 		world_play(players);
 	}
